0f5ccd78df63bea17ea6ffc62e87dfdcc4ccf2d1
alisadeghiaghili <alisadeghiaghili@gmail.com>
2023-01-17 15:46:46 +0330

    Copy source to build except secretinfo

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Welcome!"
+
+firstline=$(head -n1 ./source/changelog.md)
+read -a splitfirstline <<< $firstline
+version=${splitfirstline[1]}
+echo $version
+
+echo "Press 1 to continue or 0 to exit"
+read versioncontinue
+
+if [ $versioncontinue -eq 1 ]
+  then 
+    for file in source/*
+      do
+        if [ "$file" == "source/secretinfo.md" ]
+          then
+            echo "not copying" $file
+          else
+            echo "copying" $file
+            cp $file build
+        fi
+    done
+  else
+    echo "Please come back when you are ready"
+fi
+echo "========================================="
+cd build
+echo "listed files of version" $version "are in the build directory"
+ls -alt
+
+cd ..
